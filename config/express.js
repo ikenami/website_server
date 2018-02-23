c400c8508b95c9d5f9c91fd7e744a2aef7b3d093
Karoline Ikenami <uskyjp@gmail.com>
2018-02-23 14:40:42 -0300

    HTTPS to HTTP request

--- config/express.js
+++ config/express.js
@@ -1,9 +1,24 @@
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 var express = require('express');
 var path = require('path');
 
 module.exports = function() {
     var app = express();
-
+    
+    app.use(allowCrossDomain);
     app.use(express.static('./app/public'));
 
     app.all('/*', function(req, res) {
